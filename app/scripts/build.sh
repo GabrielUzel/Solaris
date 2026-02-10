bed502d0ed7422746ebfc1d6dde46e25c855a484
GabrielUzel <gabriel.uzel1@gmail.com>
2026-02-10 12:00:09 -0300

    refactor: updated prod build e dev enviroment

---
+++ app/scripts/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+PHOENIX_PATH="../core"
+TAURI_SRC_PATH="./src-tauri"
+
+echo "Compilando Phoenix..."
+cd $PHOENIX_PATH
+MIX_ENV=prod mix release --overwrite
+
+if [ $? -ne 0 ]; then
+    echo "❌ Erro ao compilar Phoenix"
+    exit 1
+fi
+
+cd - > /dev/null
+
+echo "Preparando Sidecar..."
+
+TARGET_TRIPLE=$(rustc -vV | sed -n 's|host: ||p')
+
+SOURCE_BIN="$PHOENIX_PATH/burrito_out/solaris_core_linux"
+DEST_BIN="$TAURI_SRC_PATH/solaris_core-$TARGET_TRIPLE"
+
+if [ ! -f "$SOURCE_BIN" ]; then
+    echo "❌ Binário do Burrito não encontrado em: $SOURCE_BIN"
+    exit 1
+fi
+
+echo "Copiando de: $SOURCE_BIN"
+echo "Para: $DEST_BIN"
+
+cp "$SOURCE_BIN" "$DEST_BIN"
+
+echo "Sidecar pronto!"
--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,5 +1,38 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use tauri_plugin_shell::ShellExt;
+
 fn main() {
-    solaris_app_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let app_data_dir = app
+                .path()
+                .app_data_dir()
+                .expect("Falha ao encontrar diretório de dados");
+
+            if !app_data_dir.exists() {
+                std::fs::create_dir_all(&app_data_dir).expect("Falha ao criar diretório de dados");
+            }
+
+            let db_path = app_data_dir.join("solaris_core_dev.db");
+            let db_path_str = db_path.to_str().unwrap().to_string();
+
+            let sidecar_command = app
+                .shell()
+                .sidecar("solaris_core")
+                .unwrap()
+                .env("DATABASE_PATH", db_path_str)
+                .env("PORT", "4000")
+                .env("PHX_SERVER", "true");
+
+            let (_rx, _child) = sidecar_command
+                .spawn()
+                .expect("Falha ao iniciar backend Phoenix");
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
