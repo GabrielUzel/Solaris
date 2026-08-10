fc663cdaf0ba7c17929162f9b6de041003e0917c
GabrielUzel <gabriel.uzel1@gmail.com>
2026-08-10 20:29:17 -0300

    chore: release 0.5.1

--- core/mix.exs
+++ core/mix.exs
@@ -4,7 +4,7 @@ defmodule SolarisCore.MixProject do
   def project do
     [
       app: :solaris_core,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
